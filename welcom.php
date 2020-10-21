9745edcf13a9c354f3f8b7d79611e309bb9e401b
Kabilan <53993282+Kabilan21@users.noreply.github.com>
2020-10-21 12:04:35 +0530

    Create welcom.php

---
+++ welcom.php
@@ -0,0 +1,8 @@
+<?php
+$username = filter_input(INPUT_POST, 'username');
+$dob = filter_input(INPUT_POST, 'dob');
+$email = filter_input(INPUT_POST, 'email');
+$password = filter_input(INPUT_POST, 'password');
+$fileinput = filter_input(INPUT_POST, 'fileinput');
+
+?>
